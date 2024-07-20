32694c0f47a91e3edeb40a4e21971a8e517af3da
THARAK HEGDE <b22mt044@iitj.ac.in>
2024-07-20 16:07:23 +0530

    React Initialization

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Home';
+import NotFound from './NotFound';
+
+function App() {
+  return (
+    <Router>
+    <div>
+      <Routes>
+        
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path='*' element={<NotFound/>}/>
+      </Routes>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
