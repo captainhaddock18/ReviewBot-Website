cd1d7967debafc4addba40de0d15b1c2299ccad4
aditya-jha-material <141742732+aditya-jha-material@users.noreply.github.com>
2024-07-20 22:22:46 +0530

    initialised tailwind css, added navbar, footer, and hero section, some more changes need to be done

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Home';
+import Header from './Header';
+import Floater from './floater';
+import Hero from './hero';
 import NotFound from './NotFound';
 
 function App() {
   return (
     <Router>
     <div>
+    <Header/>
       <Routes>
-        
-        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/" element={<Hero/>} />
         <Route exact path='*' element={<NotFound/>}/>
       </Routes>
-
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-      </h1>
+    <Floater />   
     </div>
     </Router>
   );
