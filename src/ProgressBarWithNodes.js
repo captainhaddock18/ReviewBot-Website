9e3210c1cc9351baacddfc11a25a12ee325fe796
= <=>
2024-07-21 21:17:14 +0530

    Added images to the cards

--- src/ProgressBarWithNodes.js
+++ src/ProgressBarWithNodes.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import './ProgressBarWithNodes.css'; // Ensure this path is correct
 
 const ProgressBarWithNodes = () => {
-  const [selectedNode, setSelectedNode] = useState(null);
-  const [hoveredNode, setHoveredNode] = useState(null);
-
   const nodes = [
-    { id: 1, label: 'TASK-I', info: 'We got divided into two teams and each team had to make a cool website using HTML, CSS and JavaScript.', imageUrl: 'https://scythe-potential-fa4.notion.site/Task-1-16b12bf123244a61be093c4d5b8d1fe4' },
-    { id: 2, label: 'TASK-II', info: 'We got divided into three teams and each team had to perform Exploratory Data Analysis(EDA) on the datasets provided.', imageUrl: 'https://scythe-potential-fa4.notion.site/Task-2-429c3c6753824bf58010f58a0a78a800' },
-    { id: 3, label: 'TASK-III', info: 'This was an individual task of building KNN models to understand how they work from scratch and with libraries like sklearn.', imageUrl: 'https://scythe-potential-fa4.notion.site/Task-3-4b09c35e4f664519b440ee604ba30456' },
-    { id: 4, label: 'TASK-IV', info: 'This was an individual task of building LINEAR REGRESSION and LOGISTIC REGRESSION models to understand how they work from scratch and with libraries sklearn.', imageUrl: 'https://scythe-potential-fa4.notion.site/Task-4-0d471c521b6d431a8f337710a174c2d6' },
+    { id: 1, label: 'TASK-I', info: 'We got divided into two teams and each team had to make a cool website using HTML, CSS and JavaScript.', imageUrl: 'https://img.freepik.com/free-photo/global-searching-icon-white-background_53876-65525.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1721433600&semt=ais_user' },
+    { id: 2, label: 'TASK-II', info: 'We got divided into three teams and each team had to perform Exploratory Data Analysis(EDA) on the datasets provided.', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLs9PvauxgswQln3RS_fF_xTk6b56M0t0hUQ&s' },
+    { id: 3, label: 'TASK-III', info: 'This was an individual task of building KNN models to understand how they work from scratch and with libraries like sklearn.', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Hey_Machine_Learning_Logo.png' },
+    { id: 4, label: 'TASK-IV', info: 'This was an individual task of building LINEAR REGRESSION and LOGISTIC REGRESSION models to understand how they work from scratch and with libraries sklearn.', imageUrl: 'https://cdn-icons-png.freepik.com/512/3295/3295476.png' },
   ];
+
+  // Initialize the selectedNode with the first node
+  const [selectedNode, setSelectedNode] = useState(nodes[0]);
+  const [hoveredNode, setHoveredNode] = useState(null);
 
   const handleNodeClick = (node) => {
     setSelectedNode(node);
