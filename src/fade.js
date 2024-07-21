11a89abb667822f0f65cebca4ce3912afb83980e
aditya-jha-material <141742732+aditya-jha-material@users.noreply.github.com>
2024-07-21 17:24:31 +0530

    solved the merge conflict

--- src/fade.js
+++ src/fade.js
@@ -1,6 +1,6 @@
 export default function Framer(){
     return(
-        <div class="width-100% h-screen bg-black"> hi
+        <div class="width-100% h-screeg-black"> hi
             </div>
 
     )
