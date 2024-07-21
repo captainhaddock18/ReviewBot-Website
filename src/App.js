872a10b40602cbb71ec8edba1bf546f4f91a0779
Vandita1022 <b23cy1022@iitj.ac.in>
2024-07-21 23:44:14 +0530

    change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./Header";
 
 import Floater from "./floater";
 import Hero from "./hero";
-import Performance from "./performance";
+import Performance from "./Performance";
 import NotFound from "./NotFound";
 import Progress from "./progress";
 import Teampage from "./teampage";
